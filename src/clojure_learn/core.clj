047f7b4633ebce6a1a7d71765bbdcd81388fc96c
Soumendra Daas <soumendra.daas@gmail.com>
2017-01-25 20:51:16 +0530

    Removed core.clj as it is no longer needed

--- src/clojure_learn/core.clj
+++
@@ -1,9 +0,0 @@
-(ns clojure-learn.core
-	(:require [clojure-learn.pdfviewer :refer :all])
-	(use [clojure.string :only (split triml)])
- 	(:gen-class))
-
-
-(defn -main
-  [& args]
-  (clojure-learn.modfib/process))
