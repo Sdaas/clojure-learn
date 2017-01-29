777e7c4541ab03387fbdf6b2031467b675336a3a
Soumendra Daas <soumendra.daas@gmail.com>
2017-01-29 13:13:29 +0530

    Implemented "jumping on the clouds"
    Solution for
    https://www.hackerrank.com/challenges/jumping-on-the-clouds. This is
    not a very efficient solution - It actually computes _all_ the paths
    and then prints the length of the shortest path. The question on the
    other hand just asks for the length of the shortest path ( may not need
    to compute/store each path)

--- src/clojure_learn/clouds.clj
+++ src/clojure_learn/clouds.clj
@@ -1,5 +1,6 @@
 (ns clojure-learn.clouds
   (use [clojure.string :only (split join triml)])
+  (use [clojure.set :only (union)])
   (:gen-class))
 
 ; Jumping on the Clouds
@@ -10,27 +11,49 @@
 (defn paths
     "all the paths from the first cloud to the last one"
     [clouds thunder]
+
+    (defn prepend-node-to-paths
+        "prepend current node to all paths"
+        [node paths]
+        (into #{} (map #(cons node %) paths)))
+    
     (cond
-        (first thunder)   []  ; if the first cloud is a thundercloud there is no path forward
-        (= (count clouds) 1) [clouds]
-        (= (count clouds) 2) [clouds]        
-    :else 
-        (let [
-            n0     (first clouds)
-            r1     (paths (rest clouds) (rest thunder))   ; all the possible paths from the n+1 node
-            p1     (map #(cons n0 %) r1)
-            r2     (paths (rest (rest clouds)) (rest (rest thunder))) ; all paths from the n+2 node
-            p2     (map #(cons n0 %) r2)
-            ]
-            (println p1)
-            (println r2)
-            (concat p1 p2))))
+        (first thunder)   #{}  ; if the first cloud is a thundercloud there is no path forward
+        (= (count clouds) 1) #{clouds}
+        (= (count clouds) 2) 
+            (let [
+                n0    (first clouds)
+                r1    (paths (rest clouds) (rest thunder))   ; all the possible paths from the n+1 node
+                p1    (prepend-node-to-paths n0 r1)
+                ]
+                p1)       
+        :else 
+            (let [
+                n0     (first clouds)
+                r1     (paths (rest clouds) (rest thunder))   ; all the possible paths from the n+1 node
+                p1     (prepend-node-to-paths n0 r1)
+                r2     (paths (rest (rest clouds)) (rest (rest thunder))) ; all paths from the n+2 node
+                p2     (prepend-node-to-paths n0 r2)
+                ]
+                (union p1 p2))))
+
+(defn minimum-steps
+    "Minimum steps from the first to last cloud (avoid thunder clouds)"
+    [data]
+    (let [
+        thunder (map #(= 1 %) data)   ; Which ones are thunder clouds true/false
+        clouds  (range (count data))  ; give each cloud a name
+        p       (paths clouds thunder) ; set of paths
+        lens    (map #(count %) p)         ; length of each math
+        ]
+        (dec (apply min lens))))
 
 (defn -main
   "Main program"
 	[& args]
  	(let [
-        days   (Integer/parseInt (read-line)) 
+        dummy  (read-line)
+        data   (map #(Integer/parseInt %) (split (read-line) #"\s+"))
         ]
-        (println days)))
+        (println (minimum-steps data))))
 
--- test/clojure_learn/clouds_test.clj
+++ test/clojure_learn/clouds_test.clj
@@ -14,15 +14,30 @@
   (:require [clojure.test :refer :all]
             [clojure-learn.clouds :refer :all]))
 
-(deftest paths-test
+(deftest paths2-test
 	(testing "two nodes"
-		(is (= '((1 2)) (paths [1 2] [false false]))) ; only one path
-		(is (= '() (paths [1 2] [true false]))))        ; no path
-	(testing "three nodes"
-		(is (= '((1 3) (1 2 3)) (paths [1 2 3] [false false false])))
-		(is (= '() (paths [1 2 3] [false true true])))  ; no path since last is a thunder cloud 
-		(is (= '((1 3)) (paths [1 2 3] [false true false])))  
-		(is (= '() (paths [1 2 3] [false true true]))) 
-		(is (= '() (paths [1 2 3] [true false false]))) ; no paths since first is a thunder cloud
-				))
+		(is (= #{'(1 2)} (paths [1 2] [false false]))) ; only one path
+		(is (= #{} (paths [1 2] [false true])))        ; no path
+		(is (= #{} (paths [1 2] [true false])))))     ; no path
 
+(deftest paths3-test
+	(testing "three nodes"
+		(is (= #{'(1 3) '(1 2 3)} (paths [1 2 3] [false false false])))
+		(is (= #{} (paths [1 2 3] [false true true])))  ; no path since last is a thunder cloud 
+		(is (= #{'(1 3)} (paths [1 2 3] [false true false])))  
+		(is (= #{} (paths [1 2 3] [false true true]))) 
+		(is (= #{} (paths [1 2 3] [true false false]))))); no paths since first is a thunder cloud
+
+(deftest paths-test
+	(testing "more than three nodes"
+		(is (= #{'(1 2 4 5 7)} (paths [1 2 3 4 5 6 7] [false false true false false true false])))))
+
+(deftest minimum-test
+	(testing "minimum steps"
+		(is (= 1 (minimum-steps [0 0])))
+		(is (= 1 (minimum-steps [0 0 0])))
+		(is (= 2 (minimum-steps [0 0 0 0])))  
+		(is (= 3 (minimum-steps [0 1 0 0 0 0])))
+        (is (= 4 (minimum-steps [0 0 1 0 0 1 0])))
+		(is (= 3 (minimum-steps [0 0 0 0 1 0])))
+	))
