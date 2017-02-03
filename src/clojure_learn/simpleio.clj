0c8eb39724de7b763ed3973e91fa9943db0fb22c
Soumendra Daas <soumendra.daas@gmail.com>
2017-02-03 11:52:27 +0530

    Moved all IO functions to its own namespace

---
+++ src/clojure_learn/simpleio.clj
@@ -0,0 +1,16 @@
+(ns clojure-learn.simpleio)
+
+(defn read-n
+  "read n elements from stdin into a vector"
+  [n]
+  (loop [
+         x n
+         acc []
+         ]
+    (if (= x 0)
+      acc
+      (recur (dec x) (conj acc (read-line))))))
+
+(defn write-n
+  [out]
+  (doseq [item out] (println item)))
