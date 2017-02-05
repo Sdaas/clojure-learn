2c289b09964f3e28c9a89ed1fa0538024bae9f3d
Soumendra Daas <soumendra.daas@gmail.com>
2017-02-05 23:03:32 +0530

    Implementing strange-counter. Work in progress
    Solution for https://www.hackerrank.com/challenges/strange-code

--- src/clojure_learn/strange_counter.clj
+++ src/clojure_learn/strange_counter.clj
@@ -1,1 +1,66 @@
-(ns clojure-learn.strange-counter)
+(ns clojure-learn.strange-counter
+  (use [clojure.string :only (split triml)])
+  (use [clojure-learn.util :only (exp log2)])
+  (:gen-class))
+
+
+; Strange Counter
+;
+; https://www.hackerrank.com/challenges/strange-code
+;
+; The first step is to identify which countdown sequence t belongs to
+; So we need to find  3 + 6 + 12 + …. such that is it just greater than t
+;
+; For example:
+; With t = 15
+; 3 + 6 + 12 is just larger than 15, so its part of the 3rd sequence
+;
+; This is a geometric sequence with a = 3 and r = 2
+; Given g(n) = a ( 1 - r^n ) / ( 1 - r ) = 3*( 2^n - 1 )
+; g(N) <= t < g(N+1)
+;
+; t > 3*(2^N - 1 ) => n = log 2 ( t/3 + 1 )
+
+
+
+(defn countdown-sequence
+  "Compute the countdown sequence that t is part of"
+  [t]
+  (let [
+        n-exact (log2 (inc (/ t 3.00)))
+        n1 (int (Math/ceil n-exact))
+        ]
+    n1))
+
+
+(defn value-at-start-of-sequence
+  "the value at start of the nth countdown sequence"
+  ;3*2^(n-1)
+  [n]
+  (* 3 (exp 2 (dec n))))
+
+(defn time-at-start-of-sequence
+  "the time at str"
+  [n]
+  (inc (* 3 (dec (exp 2 (dec n))))))
+
+
+(defn display-value
+  "the value displayed at time t"
+  [t]
+  (let [
+        n (countdown-sequence t)
+        v0 (value-at-start-of-sequence n)
+        t0 (time-at-start-of-sequence n)
+        dt (- t t0)
+        ]
+    (- v0 dt)))
+
+(defn -main
+  "Main loop"
+  [& args]
+  (let [
+        n (Integer/parseInt (read-line))
+        ]
+    (println (display-value n))))
+
--- test/clojure_learn/strange_counter_test.clj
+++ test/clojure_learn/strange_counter_test.clj
@@ -1,1 +1,57 @@
-(ns clojure-learn.strange-counter-test)
+(ns clojure-learn.strange-counter-test
+  (:require [clojure.test :refer :all]
+            [clojure-learn.strange-counter :refer :all]))
+
+;Note that the test must be in a XXX_test file (underscore)
+;
+;To run all tests in project
+;  lein test
+;
+;To run all tests for strange-counter.clj
+;	lein test clojure-learn.strange-counter-test
+;
+;To run only a specific test
+;   lein test :only clojure-learn.strange-counter-test/foo
+;
+
+(deftest countdown-test
+  (testing "tests for countdown sequence test"
+    (is (= 1 (countdown-sequence 1)))
+    (is (= 1 (countdown-sequence 3)))
+    (is (= 2 (countdown-sequence 4)))
+    (is (= 2 (countdown-sequence 9)))
+    (is (= 3 (countdown-sequence 10)))
+    (is (= 3 (countdown-sequence 15)))
+    (is (= 3 (countdown-sequence 21)))
+    (is (= 4 (countdown-sequence 22)))
+    ))
+
+(deftest start-test
+  (testing "tests for value at the start of a sequence"
+    (is (= 3 (value-at-start-of-sequence 1)))
+    (is (= 6 (value-at-start-of-sequence 2)))
+    (is (= 12 (value-at-start-of-sequence 3)))
+    (is (= 24 (value-at-start-of-sequence 4)))
+    ))
+
+(deftest time-test
+  (testing "tests for time at the start of a sequence"
+    (is (= 1 (time-at-start-of-sequence 1)))
+    (is (= 4 (time-at-start-of-sequence 2)))
+    (is (= 10 (time-at-start-of-sequence 3)))
+    (is (= 22 (time-at-start-of-sequence 4)))
+    ))
+
+(deftest display-test
+  (testing "tests for time at the start of a sequence"
+    (is (= 3 (display-value 1)))
+    (is (= 1 (display-value 3)))
+    (is (= 6 (display-value 4)))
+    (is (= 1 (display-value 9)))
+    (is (= 12 (display-value 10)))
+    (is (= 7 (display-value 15)))
+    (is (= 1 (display-value 21)))
+    (is (= 24 (display-value 22)))
+    ))
+
+
