422c018742535709b5a84e16bc36551ae41104db
Soumendra Daas <soumendra.daas@gmail.com>
2017-02-11 13:34:14 +0530

    modified descending() to return zero-based offsets. updated unit tests accordingly fix() is work in progress

--- src/clojure_learn/almost_sorted.clj
+++ src/clojure_learn/almost_sorted.clj
@@ -8,10 +8,21 @@
 ; https://www.hackerrank.com/challenges/almost-sorted
 ;
 
-; if second > first => subsequence(list)
+; In a sequence x1 ... xk y1 .... yk z1 ... zk
+; where x1 .. xk is monotonically increasing
+; and   z1 .. zk is monotonicall increasing
+; and   y1 .. yk is monotonically decreasing
+;
+; it is possible convert the sequence by reversing y1..yk only if
+; * y1 .. yk is monotonically decreasing
+; and yk > xk
+; and y1 < z1
+;
+; [10 20 40 30 50 ] => descending sequence is [40 30]. 40 < 50 and 30 > 20
+; [30 10 20]        => descending sequence is [30 10]. 30 < 20 FALSE. so it is not possible to conver the sequence
 
 (defn descending
-  "find the index (offset 1 based) of the descending subsequence. Returns nil if no descending sequence is gound"
+  "find the index (offset 0 based) of the descending subsequence. Returns nil if no descending sequence is gound"
   ([data] (descending data 0))
   ([data index]
 
@@ -36,9 +47,25 @@
            ]
        (if (= (inc idx1) (count data))
          nil  ; This is a purely ascending sequence. So no match
-         {:start (inc idx1) :end (inc idx3)})))))
+         {:start idx1 :end idx3})))))
 
+(defn fix
+  [data]
 
+  (defn -operation
+    [len]
+    (cond
+      (= 0 len) "no"
+      (= 1 len) "swap"
+      :else "reverse"))
+
+  (let [
+        {start :start end :end} (or (descending data) {:start 0 :end 0})
+        ;c1     ; yk > xk
+        ;fixable? (and (> (data end) (data (dec start))) (< (data start) (data (inc end))))  ; yk > xk and y1 < z1
+        op (-operation (- end start))
+        ]
+    {:op op :start start :end end}))
 
 (defn -main
   "Main loop"
--- test/clojure_learn/almost_sorted_test.clj
+++ test/clojure_learn/almost_sorted_test.clj
@@ -20,14 +20,35 @@
     (is (nil? (descending [1])))                 ; no descending numbers
     (is (nil? (descending [1 2])))                 ; no descending numbers
     (is (nil? (descending (range 10))))         ; no descending numbers
-    (is (= {:start 3 :end 5} (descending [1 2 5 4 3 6])))
-    (is (= {:start 5 :end 6} (descending [1 2 3 4 6 5])))   ; last two numbers are descending
-    (is (= {:start 4 :end 6} (descending [1 2 3 6 5 4])))   ; last three numbers are descending
-    (is (= {:start 1 :end 2} (descending [2 1 3 4 5 6])))   ; first two numbers are descending
-    (is (= {:start 1 :end 4} (descending [4 3 2 1 5 6])))   ; first four numbers are descending
-    (is (= {:start 1 :end 2} (descending [2 1])))
-    (is (= {:start 1 :end 3} (descending [3 2 1])))
-    (is (= {:start 1 :end 6} (descending [6 5 4 3 2 1])))   ; the entire sequence is desceding
-    (is (= {:start 3 :end 5} (descending [12 17 25 23 20 29 32 40])))  ; .. 25 23 20 ... is the sequence
+    (is (= {:start 2 :end 4} (descending [1 2 5 4 3 6])))
+    (is (= {:start 4 :end 5} (descending [1 2 3 4 6 5])))   ; last two numbers are descending
+    (is (= {:start 3 :end 5} (descending [1 2 3 6 5 4])))   ; last three numbers are descending
+    (is (= {:start 0 :end 1} (descending [2 1 3 4 5 6])))   ; first two numbers are descending
+    (is (= {:start 0 :end 3} (descending [4 3 2 1 5 6])))   ; first four numbers are descending
+    (is (= {:start 0 :end 1} (descending [2 1])))
+    (is (= {:start 0 :end 2} (descending [3 2 1])))
+    (is (= {:start 0 :end 5} (descending [6 5 4 3 2 1])))   ; the entire sequence is desceding
+    (is (= {:start 2 :end 4} (descending [12 17 25 23 20 29 32 40])))  ; .. 25 23 20 ... is the sequence
     ))
 
+(deftest fix-test
+  (testing "cannot be fixed - since it is already sorted"
+    (is (= {:op "no" :start 0 :end 0} (fix [])))     ; empty list
+    (is (= {:op "no" :start 0 :end 0} (fix [1 2]))) ; already sorted
+    (is (= {:op "no" :start 0 :end 0} (fix (range 10)))) ; already sorted
+    )
+  (testing "cannot be fixed"
+    (is (= {:op "no" :start 0 :end 0} (fix [ 30 10 20 ])))     ; descending sequence in start
+    (is (= {:op "no" :start 0 :end 0} (fix [ 1 5 30 10 20 ]))) ; descending sequence in middle
+    (is (= {:op "no" :start 0 :end 0} (fix [ 1 5 20 30 10 ]))) ; descending sequence in end
+    )
+  (testing "can be fixed by swapping"
+    (is (= {:op "swap" :start 1 :end 2} (fix [20 10 30 40 50 ]))) ; swap the first two
+    (is (= {:op "swap" :start 4 :end 5} (fix [10 20 30 50 40 ]))) ; swap the last two
+    (is (= {:op "swap" :start 3 :end 4} (fix [10 20 40 30 50 ]))) ; swap the ones in middle
+    )
+  (testing "can be fixed by reversing"
+    (is (= {:op "reverse" :start 1 :end 3} (fix [30 20 10 40 50 60]))) ; reverse a sequence at start
+    (is (= {:op "reverse" :start 4 :end 6} (fix [10 20 30 60 50 40]))) ; reverse a sequence at end
+    (is (= {:op "reverse" :start 3 :end 5} (fix [10 20 50 40 30 60]))) ; reverse a sequence in middle
+    ))
